21bf9aad4d2eb27dee116ca9886364673dd4bb82
Emilio Heinzmann <emiliosheinz@gmail.com>
2026-05-15 09:36:35 -0300

    chore: drop migrations module

--- apps/api/src/migrations/migrations.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-import { MigrationsController } from "./migrations.controller";
-import { MigrationsService } from "./migrations.service";
-
-@Module({
-  controllers: [MigrationsController],
-  providers: [MigrationsService],
-})
-export class MigrationsModule {}
